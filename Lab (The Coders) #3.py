96839c0fc6c27678308c92dd410b0306f6131272
ashleygoce <94890040+sreneee@users.noreply.github.com>
2022-04-17 21:37:00 +0800

    Add files via upload

---
+++ Lab (The Coders) #3.py
@@ -0,0 +1,10 @@
+from tkinter import *
+win = Tk()
+win.geometry("450x200+10+20")
+win.title("Father of Computer (The Coder)")
+
+lbl = Label(win, text = "Charles Babbage", fg = "black", bg = "cyan", font = ("verdana",25))
+lbl.place(x=70,y=70)
+
+
+win.mainloop()
